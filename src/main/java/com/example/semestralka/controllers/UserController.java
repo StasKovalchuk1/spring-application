bd2c0bbd194b0bee1b4d920ee9655ff2d80db3b2
StasKovalchuk1 <64376198+StasKovalchuk1@users.noreply.github.com>
2023-11-25 23:50:56 +0100

    add user controller

--- src/main/java/com/example/semestralka/controllers/UserController.java
+++ src/main/java/com/example/semestralka/controllers/UserController.java
@@ -1,9 +1,47 @@
 package com.example.semestralka.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.semestralka.controllers.util.RestUtils;
+import com.example.semestralka.model.Event;
+import com.example.semestralka.model.User;
+import com.example.semestralka.security.model.UserDetails;
+import com.example.semestralka.services.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("rest/myProfile")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> register(@RequestBody User user) {
+        userService.save(user);
+        final HttpHeaders headers = RestUtils.createLocationHeaderFromCurrentUri("/current");
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/current", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User getCurrent(Authentication auth) {
+        assert auth.getPrincipal() instanceof UserDetails;
+        return ((UserDetails) auth.getPrincipal()).getUser();
+    }
+
+    @GetMapping("/favorites")
+    public ResponseEntity<List<Event>> getFavoritesByUserId(@RequestBody User user) {
+        List<Event> favorites = userService.getAllFavoriteEvents(user);
+        return new ResponseEntity<>(favorites, HttpStatus.OK);
+    }
 }
